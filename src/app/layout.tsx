ba7562e731ce2e0e6d33ac0c6aab765a0d3cf973
oilater <oilater@naver.com>
2025-12-08 15:21:56 +0900

    feat: biome 적용

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Next.js App',
+  description: 'Next.js app with Vanilla-extract',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
