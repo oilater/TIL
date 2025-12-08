f7dffd1734e7af247f4c7af1653432627ba034a9
oilater <oilater@naver.com>
2025-12-09 04:47:38 +0900

    feat: 메타태그 임시 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,22 @@ import { LayoutHeader } from '@/shared/components/Header/LayoutHeader';
 
 export const metadata: Metadata = {
   title: 'TIL',
-  description: 'TIL',
+  description: '퇴근 후 5분만 투자해 오늘 한 일을 기록해보세요!',
+  authors: {
+    name: '김성현',
+    url: 'https://www.linkedin.com/in/seonghyeonkim',
+  },
+  keywords: [
+    'TIL',
+    'today i learned',
+    'today i did',
+    'til application',
+    'til app',
+  ],
+  viewport: 'width=device-width, initial-scale=1.0',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
