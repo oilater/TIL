3339edde982ca900c3431dd1890b20d310871cad
oilater <oilater@naver.com>
2025-12-09 04:01:40 +0900

    feat: middleware 구현

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const sessionCookie = request.cookies.get('session');
+  const { pathname } = request.nextUrl;
+
+  if (pathname === '/login') {
+    if (sessionCookie) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (pathname === '/') {
+    if (!sessionCookie) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/', '/login'],
+};
