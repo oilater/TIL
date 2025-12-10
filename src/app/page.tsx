df39ee9202524b1150b1ac5e57111c7503b8cf43
oilater <oilater@naver.com>
2025-12-10 13:03:34 +0900

    refactor: 렌더링 로직 삼항연산자로 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,21 @@
 'use client';
-import './globals.css';
 
+import './globals.css';
 import ConnectRepoPage from './repo/components/ConnectPage';
 import WritePage from './repo/components/WritePage';
 import { useRepos } from './repo/hooks/query/useRepos';
 
 export default function Home() {
   const { data: repo, isLoading } = useRepos();
+  const hasRepoConnected = repo?.repoName !== null;
 
   if (isLoading) {
     return (
       <main>
-        <div>로딩 중...</div>
+        <div>Loading...</div>
       </main>
     );
   }
 
-  if (repo?.repoName) {
-    return (
-      <main>
-        <WritePage />
-      </main>
-    );
-  }
-
-  return (
-    <main>
-      <ConnectRepoPage />
-    </main>
-  );
+  return hasRepoConnected ? <WritePage /> : <ConnectRepoPage />;
 }
